287a6d75d012c3b938a2a56eae0ac67770499352
ruanye <ruanye@88.com>
2022-08-02 14:04:15 +0800

    脚本修改

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dburl: 'mongodb+srv://ruanye:yezui1314@cluster0.vgqkh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  dburl: "mongodb://admin:123456@43.138.50.102:27017/let",
   defaultPageSize: 20, //默认分页大小
   defaultPageNum: 1     //默认分页页码
 }; 
